b954ffbbe13ba387e1d23149c0bd07ec6f54d5a8
Ryan Brainard <brainard@heroku.com>
2012-02-01 01:40:33 -0800

    ugly, but working table of apps

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
 gem 'sinatra', '1.1.0'
 gem 'thin', '1.2.7'
+gem 'heroku-rb', '0.0.1', :git => 'git@github.com:heroku/heroku.rb.git'
--- web.rb
+++ web.rb
@@ -1,5 +1,38 @@
 require 'sinatra'
+require 'heroku-api'
 
-get '/' do
-    "Hello, world"
+enable :sessions
+
+get '/apps' do
+  heroku = Heroku::API.new(:api_key => session[:api_key])
+
+  apps = heroku.get_apps.body
+
+  output = "<table border='1'>"
+
+  output << "<tr>"
+  apps[0].each { |header| output << "<th>#{header[0]}</th>" }
+  output << "</tr>"
+
+  apps.each do |app|
+    output << "<tr>"
+    app.each do |prop|
+      output << "<td>#{prop[1]}</td>"
+    end
+    output << "</tr>"
+  end
+  output << "</table>"
+end
+
+get '/login' do
+  "<form method='post'>API Key: <input name='api_key'/><input type='submit'/></form>"
+end
+
+post '/login' do
+  session[:api_key] = params[:api_key]
+  redirect "/apps"
+end
+
+get '/*' do
+  redirect "/login"
 end
