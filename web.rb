c09ad6d5efde005272b8b3886af2e65e925dc066
Ryan Brainard <brainard@heroku.com>
2012-02-01 02:14:19 -0800

    allow apps to be deleted. all super ugly from the routes file :(

--- web.rb
+++ web.rb
@@ -3,25 +3,50 @@ require 'heroku-api'
 
 enable :sessions
 
+post '/apps' do
+  output = ""
+  heroku = Heroku::API.new(:api_key => session[:api_key])
+  appAction = params[:action]
+  params.each do |param|
+    if param[0].match(/^actionable\//)
+      appName =  param[0].split('/')[1]
+
+      if appAction == 'delete'
+        begin
+          heroku.delete_app(appName)
+          output << "<p>Deleted #{appName}</p>"
+        rescue
+          output << "<p>Failed to delete #{appName}</p>"
+        end
+      end
+    end
+  end
+  output
+end
+
 get '/apps' do
   heroku = Heroku::API.new(:api_key => session[:api_key])
 
   apps = heroku.get_apps.body
 
-  output = "<table border='1'>"
+  output = "<form method='post'>"
+  #output << "<input type='submit' name='action' value='view'>"
+  output << "<input type='submit' name='action' value='delete'>"
+  output << "<table border='1'>"
 
-  output << "<tr>"
+  output << "<tr><th>Action</th>"
   apps[0].each { |header| output << "<th>#{header[0]}</th>" }
   output << "</tr>"
 
   apps.each do |app|
-    output << "<tr>"
+    output << "<tr><td><input type='checkbox' name='actionable/#{app['name']}'/></td>"
     app.each do |prop|
       output << "<td>#{prop[1]}</td>"
     end
     output << "</tr>"
   end
   output << "</table>"
+  output << "</form>"
 end
 
 get '/login' do
